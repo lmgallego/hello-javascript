4b0c52e612a2a0bda35e04c167699d4555c8ed50
lmgallego <luisma.gallego@gmail.com>
2024-07-10 21:57:17 +0200

    Primera Clase

---
+++ Basic/00-helloworld.js
@@ -0,0 +1,20 @@
+// Esto es un comentario simple
+
+/*
+Esto es 
+un comentario
+en varias líneas
+*/
+
+console.log("Hola Mundo")
+console.log('Hola Mundo')
+console.log(`Hola Javascript`)
+
+// console.log("Hola Mundo")
+
+console.log("5")
+console.log(5)
+console.log(5 + 2)
+console.log(5 - 2)
+console.log(5*2)
+console.log(5 ** 2)
---
+++ Basic/01-variables.js
@@ -0,0 +1,31 @@
+// var
+
+var  helloWorld = "Hello Javascript"
+console.log(helloWorld)
+
+helloWorld = "Hola de nuevo, Javascript!"
+console.log(helloWorld)
+
+
+
+// let
+
+
+let  helloWorld2 = "Hello Javascript 2"
+console.log(helloWorld2)
+
+helloWorld2 = "Hola de nuevo, Javascript 2!"
+console.log(helloWorld2)
+
+
+
+// const
+
+// const no puede ser reasignada
+
+const  helloWorld3 = "Hello Javascript 3"
+console.log(helloWorld3)
+
+// Error
+// helloWorld3 = "Hola de nuevo, Javascript 2!"
+// console.log(helloWorld3)
