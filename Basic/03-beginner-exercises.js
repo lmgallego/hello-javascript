a4301932a87d85de5dcbef87aafeca519c678bab
lmgallego <luisma.gallego@gmail.com>
2024-07-31 20:10:42 +0200

    Ejercicios

--- Basic/03-beginner-exercises.js
+++ Basic/03-beginner-exercises.js
@@ -6,13 +6,31 @@ https://www.twitch.tv/videos/2200149072?t=00h08m02s
 
 // 1. Escribe un comentario en una línea
 
+// Esto es un comentario de una sola linea
+
 // 2. Escribe un comentario en varias líneas
+
+/* Comentario de
+varias líneas
+*/
 
 // 3. Declara variables con valores asociados a todos los datos de tipo primitivos
 
+let nombre = "Luisma"
+let edad = 48
+let esMasculino = true
+
 // 4. Imprime por consola el valor de todas las variables
 
+console.log(nombre)
+console.log(edad)
+console.log(esMasculino)
+
 // 5. Imprime por consola el tipo de todas las variables
+
+console.log(typeof nombre)
+console.log(typeof edad)
+console.log(typeof esMasculino)
 
 // 6. A continuación, modifica los valores de las variables por otros del mismo tipo
 
--- Basic/09-conditionals-exercises.js
+++ Basic/09-conditionals-exercises.js
@@ -8,14 +8,53 @@ https://www.twitch.tv/videos/2206228701?t=00h16m02s
 
 // 1. Imprime por consola tu nombre si una variable toma su valor
 
+let nombre = "Luisma"
+if (nombre == "Luisma") {
+    console.log(nombre)
+}
+
 // 2. Imprime por consola un mensaje si el usuario y contraseña concide con unos establecidos
+
+let usuario = "luisma"
+let pass = 1234
+if (usuario == "luisma" && pass == 1234) {
+    console.log("Acceso autorizado")
+} else {
+    console.log("Acceso no autorizado")
+}
+
 
 // 3. Verifica si un número es positivo, negativo o cero e imprime un mensaje
 
+let num = 12
+
+if (num < 0) {
+    console.log("El número es negativo")
+} else if (num == 0) {
+    console.log("El número es cero")  
+} else {
+    console.log("El número es positivo")
+}
+
+
 // 4. Verifica si una persona puede votar o no (mayor o igual a 18) e indica cuántos años le faltan
+
+let edad = 18
+let mayorEdad = 18
+let añosFaltantes = mayorEdad - edad
+
+if (edad < 18) {
+    console.log("No puedes votar te faltan " + añosFaltantes + " años")
+} else if (edad >= 18) {
+    console.log("Puedes votar")
+}
 
 // 5. Usa el operador ternario para asignar el valor "adulto" o "menor" a una variable
 //    dependiendo de la edad 
+
+let anios = 16
+let mensaje = anios >= 18 ? "Adulto" : "Menor"
+console.log(mensaje)
 
 // 6. Muestra en que estación del año nos encontramos dependiendo del valor de una variable "mes"
 
